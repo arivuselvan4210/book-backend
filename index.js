7df40c40cc15382894db5f8a25ff41ff67a1f41c
Arivu <46324212+arivuselvan4210@users.noreply.github.com>
2024-02-29 23:57:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ app.use(cors({
    origin: 'https://blog-forntend.vercel.app',
    optionsSuccessStatus: 200,
 }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://book-forntend-rho.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use("/", books);
 mongoss
   .connect(
